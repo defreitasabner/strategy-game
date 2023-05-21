c1be7eca98085af659c782f17841f0bb553e3d08
Abner Freitas <defreitasabner@gmail.com>
2023-05-21 18:09:44 -0300

    Testing some pytest features

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import random
 
 import pygame
+import numpy as np
 
 from constants import colors, settings
 
@@ -16,6 +17,12 @@ for _ in range(10):
     obstacle_rect = pygame.Rect((random.randint(0, 500), random.randint(0, 300), 25, 25))
     obstacles.append(obstacle_rect)
 
+# Variables
+player_selected = False
+movement = None
+partial_movement = None
+is_moving = False
+
 # Game Loop
 game_run = True
 while game_run:
@@ -23,15 +30,27 @@ while game_run:
     # Cleaning screen
     screen.fill(colors.BACKGROUND)
     
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_left_clicked = pygame.mouse.get_pressed()[0]
+    mouse_right_clicked = pygame.mouse.get_pressed()[2]
+
     # Handling events
     player_color = colors.GREEN
     if player.collidelist(obstacles) >= 0:
         player_color = colors.RED
+    if player.collidepoint(mouse_pos):
+        player_color = colors.WHITE
+        if mouse_left_clicked:
+            player_selected = True
+    if mouse_left_clicked and not player.collidepoint(mouse_pos):
+        player_selected = False
 
     # Drawing actors
     pygame.draw.rect(screen, player_color, player)
     for obstacle in obstacles:
         pygame.draw.rect(screen, colors.BLUE, obstacle)
+    if player_selected:
+        pygame.draw.circle(screen, colors.WHITE, player.center, 25, 2)
 
     # Check player movements
     key = pygame.key.get_pressed()
@@ -43,6 +62,20 @@ while game_run:
         player.move_ip(0, -1)
     elif key[pygame.K_s]:
         player.move_ip(0, 1)
+    
+    if player_selected and mouse_right_clicked:
+        target_pos = np.array(pygame.mouse.get_pos())
+        np_player_pos = np.array(player.center)
+        movement = tuple(target_pos - np_player_pos)
+        is_moving = True
+
+    if is_moving:
+        pygame.draw.circle(screen, colors.RED, target_pos, 10, 2)
+        pygame.draw.line(screen, colors.RED, player.center, target_pos)
+        if player.center != tuple(target_pos):
+            player.move_ip(movement)
+        else:
+            is_moving = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
